17919151f38d42cfb5a194ac2fd27386fcbabda9
Ilia Zaraysky <nextontext@yandex.ru>
2021-09-15 17:28:49 +0300

    Add custom user models

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,16 @@
 from django.conf import settings
 from django.urls import path, include
 
+from users import views
+
 # http://127.0.0.1:8000/api/auth/token/login/
+
+app_name = 'users'
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     # path('auth/', include('djoser.urls.authtoken')),
-    path('auth/', include('djoser.urls.jwt'))
+    path('auth/', include('djoser.urls.jwt')),
+    path('users/', views.RegisterView.as_view(), name='auth_register'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from rest_framework import status, viewsets
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from users.serializers import RegisterSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    pass
+class RegisterView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny, )
+    serializer_class = RegisterSerializer
+
+
+
+
